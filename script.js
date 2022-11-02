97ae6a9530c195b7e64bc447c74a749006b249f4
Ariga Yeswanth Kumar <102847173+Yeswanthariga@users.noreply.github.com>
2022-11-02 11:08:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function script() {
   return (
     <div >
-      <button id='btn'>Submit button</button>
+      <button id='btn'>Submit</button>
     </div>
   );
 }
